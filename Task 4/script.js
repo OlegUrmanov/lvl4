de823de27bd956c1cfc2c2bedc1291d5a6425c0d
OlegUrmanov <43071954+OlegUrmanov@users.noreply.github.com>
2019-02-08 18:58:15 +0300

    Add files via upload
    Add test function and finalized getUserProfileFromObj function.

--- Task 4/script.js
+++ Task 4/script.js
@@ -1,11 +1,37 @@
 function getUserProfileFromObj(object) {
   if (typeof(object) === "object" && object != null) {
     if ((typeof object['username'] !== "undefined") && (typeof object['email'] !== "undefined")) {
-        return {
-          username: object.username,
-          email: object.email,
-        };
+      return {
+        username: object.username,
+        email: object.email,
+      };
     }
   }
-  return null;
+  return false;
+}
+
+function _test() {
+
+
+  console.log(getUserProfileFromObj({
+    id: 1,
+    username: 'Vasya',
+    email: 'user@mail.ru'
+  }));
+
+
+
+  console.log(getUserProfileFromObj({
+    uwsername: 'Vasya',
+    email: 'user@mail.ru'
+  }));
+
+
+  console.log(getUserProfileFromObj({
+    qw: 1,
+    uwsername: 'Vasya',
+    email: 'user@mail.ru'
+  }));
+
+
 }
